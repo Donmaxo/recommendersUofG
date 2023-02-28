b419f286f103bf451135088428ca02ee334430d8
Donmaxo <maximilian.donar@gmail.com>
2023-02-28 21:36:27 +0000

    added pr_matrix v1

--- examples/00_quick_start/predict_on_nrms.py
+++ examples/00_quick_start/predict_on_nrms.py
@@ -86,17 +86,17 @@ model.model.load_weights(os.path.join(model_path, "nrms_ckpt"))
 # print(model.run_fast_eval(valid_news_file, valid_behaviors_file))
 group_impr_indexes, group_labels, group_preds, gp_reldiff = model.run_fast_eval(test_news_file, test_behaviors_file, update=True)
 
-with open(os.path.join(data_path, 'prediction.txt'), 'w') as f:
-    for impr_index, preds in tqdm(zip(group_impr_indexes, group_preds)):
-        impr_index += 1
-        pred_rank = (np.argsort(np.argsort(preds)[::-1]) + 1).tolist()
-        pred_rank = '[' + ','.join([str(i) for i in pred_rank]) + ']'
-        f.write(' '.join([str(impr_index), pred_rank])+ '\n')
+# with open(os.path.join(data_path, 'prediction.txt'), 'w') as f:
+#     for impr_index, preds in tqdm(zip(group_impr_indexes, group_preds)):
+#         impr_index += 1
+#         pred_rank = (np.argsort(np.argsort(preds)[::-1]) + 1).tolist()
+#         pred_rank = '[' + ','.join([str(i) for i in pred_rank]) + ']'
+#         f.write(' '.join([str(impr_index), pred_rank])+ '\n')
 
 
-f = zipfile.ZipFile(os.path.join(data_path, 'prediction-nrms-fit.zip'), 'w', zipfile.ZIP_DEFLATED)
-f.write(os.path.join(data_path, 'prediction.txt'), arcname='prediction.txt')
-f.close()
+# f = zipfile.ZipFile(os.path.join(data_path, 'prediction-nrms-fit.zip'), 'w', zipfile.ZIP_DEFLATED)
+# f.write(os.path.join(data_path, 'prediction.txt'), arcname='prediction.txt')
+# f.close()
 
 with open(os.path.join(data_path, 'prediction_reldiff.txt'), 'w') as f:
     for impr_index, preds in tqdm(zip(group_impr_indexes, gp_reldiff)):
@@ -105,7 +105,7 @@ with open(os.path.join(data_path, 'prediction_reldiff.txt'), 'w') as f:
         pred_rank = '[' + ','.join([str(i) for i in pred_rank]) + ']'
         f.write(' '.join([str(impr_index), pred_rank])+ '\n')
 
-f = zipfile.ZipFile(os.path.join(data_path, 'prediction_reldiff-nrms-fit-24.zip'), 'w', zipfile.ZIP_DEFLATED)
+f = zipfile.ZipFile(os.path.join(data_path, 'prediction_reldiff-nrms-fit-v2-10-pr.zip'), 'w', zipfile.ZIP_DEFLATED)
 f.write(os.path.join(data_path, 'prediction_reldiff.txt'), arcname='prediction.txt')
 f.close()
 
