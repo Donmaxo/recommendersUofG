3fa7a71721e84d0ec556ff375975694c2b8c19a9
Donmaxo <maximilian.donar@gmail.com>
2023-03-15 12:52:00 +0000

    tsnet v2.4

--- examples/00_quick_start/tsnet.py
+++ examples/00_quick_start/tsnet.py
@@ -56,9 +56,9 @@ def run(name, i):
     )
 
     uh_embd = otsne.fit(np.array(uh))
-    u_embd = uh_embd.transform(np.array(u))
-    ur_embd = uh_embd.transform(np.array(ur[i]))
-    cn_embd = uh_embd.transform(np.array(cn[i]))
+    u_embd = uh_embd.transform(np.array([u]))
+    ur_embd = uh_embd.transform(np.array([ur[i]]))
+    cn_embd = uh_embd.transform(np.array([cn[i]]))
 
     fig, ax = plt.sublots(figsize=(8,8))
     utils.plot(uh_embd, uh, colors=utils.MACOSKO_COLORS)
